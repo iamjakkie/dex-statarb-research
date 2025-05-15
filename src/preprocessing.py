cb99b50a775d349d287c45c0e1a69c9bc62a543c
jakkie <jakubkielbasiewicz@gmail.com>
2025-05-15 10:15:26 +0200

    solana exchange stats

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,2 +1,19 @@
 # bucket 1min (vwap)
 # normalize prices
+
+import polars as pl
+
+def compute_exchange_stats(src: str, lf: pl.LazyFrame) -> pl.DataFrame:
+    if src == "SOLANA":
+        stats_lf = (
+            lf
+            .group_by(["EXCHANGE", "QUOTE_ASSET"])
+            .agg([
+                pl.count().alias("count"),
+                pl.col("block_time").min().alias("start_date"),
+                pl.col("block_time").max().alias("end_date"),
+            ])
+            .sort(["EXCHANGE", "QUOTE_ASSET"])
+        )
+    
+    return stats_lf.collect()
