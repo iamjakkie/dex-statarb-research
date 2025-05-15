573728adc2bc202f44a84237154627a8dbb84f17
jakkie <jakubkielbasiewicz@gmail.com>
2025-05-15 16:37:22 +0200

    preprocessing to exclude not big enough dexes

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,7 @@
 # bucket 1min (vwap)
 # normalize prices
 
+import numpy as np
 import polars as pl
 
 def compute_exchange_stats(src: str, lf: pl.LazyFrame) -> pl.DataFrame:
@@ -16,4 +17,12 @@ def compute_exchange_stats(src: str, lf: pl.LazyFrame) -> pl.DataFrame:
             .sort(["EXCHANGE", "QUOTE_ASSET"])
         )
     
-    return stats_lf.collect()
+    stats_df = stats_lf.collect()
+    counts = stats_df["count"].to_numpy()
+    q1, q3 = np.percentile(counts, [25, 75])
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    if lower_bound <= 0:
+        lower_bound = q1
+
+    return stats_df.filter(pl.col("count") > lower_bound)
