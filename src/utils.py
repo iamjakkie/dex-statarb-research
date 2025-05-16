f1423c1d658c7ee4107a08ec5aaa1899fa686351
jakkie <jakubkielbasiewicz@gmail.com>
2025-05-16 08:15:17 +0200

    import clarification

--- src/utils.py
+++ src/utils.py
@@ -1,41 +1,41 @@
 # load s3
 # fetch token data (if possible)
 
-from tokens import TOKEN_MAPPING
+import tokens
 
 import polars as pl
 
 def load_data(src: str, token: str) -> pl.LazyFrame:
 
     if src == "SOLANA":
-        token_clean = TOKEN_MAPPING[token]['sol']
+        token_clean = tokens.TOKEN_MAPPING[token]['sol']
         if not token_clean:
-            raise ValueError(f"Token {token} not found in TOKEN_MAPPING for SOLANA")
+            raise ValueError(f"Token {token} not found in tokens.TOKEN_MAPPING for SOLANA")
         s3_path = f"s3://iamjakkie-public/normalized/solana_swaps/*/PROGRAM_ID=*/TOKEN={token_clean}/QUOTE_ASSET=*/*.parquet"
     elif src == "ETHEREUM":
-        token_clean = TOKEN_MAPPING[token]['eth']
+        token_clean = tokens.TOKEN_MAPPING[token]['eth']
         if not token_clean:
-            raise ValueError(f"Token {token} not found in TOKEN_MAPPING for ETHEREUM")
+            raise ValueError(f"Token {token} not found in tokens.TOKEN_MAPPING for ETHEREUM")
         s3_path = f"s3://iamjakkie-public/normalized/ethereum_swaps/*/PLATFORM=*/TOKEN={token_clean}/*.parquet"
     elif src == "BASE":
-        token_clean = TOKEN_MAPPING[token]['base']
+        token_clean = tokens.TOKEN_MAPPING[token]['base']
         if not token_clean:
-            raise ValueError(f"Token {token} not found in TOKEN_MAPPING for BASE")
+            raise ValueError(f"Token {token} not found in tokens.TOKEN_MAPPING for BASE")
         s3_path = f"s3://iamjakkie-public/normalized/base_swaps/*/PLATFORM=*/TOKEN={token_clean}/*.parquet"
     elif src == "DYDX":
-        token_clean = TOKEN_MAPPING[token]['dydx']
+        token_clean = tokens.TOKEN_MAPPING[token]['dydx']
         if not token_clean:
-            raise ValueError(f"Token {token} not found in TOKEN_MAPPING for DYDX")
+            raise ValueError(f"Token {token} not found in tokens.TOKEN_MAPPING for DYDX")
         s3_path = f"s3://iamjakkie-public/dydx/candles/{token_clean}.parquet"
     elif src == "HYPERLIQUID":
-        token_clean = TOKEN_MAPPING[token]['hyperliquid']
+        token_clean = tokens.TOKEN_MAPPING[token]['hyperliquid']
         if not token_clean:
-            raise ValueError(f"Token {token} not found in TOKEN_MAPPING for HYPERLIQUID")
+            raise ValueError(f"Token {token} not found in tokens.TOKEN_MAPPING for HYPERLIQUID")
         s3_path = f"s3://iamjakkie-public/hyperliquid/*/coin={token_clean}/*.parquet"
     elif src == "DRIFT":
-        token_clean = TOKEN_MAPPING[token]['drift']
+        token_clean = tokens.TOKEN_MAPPING[token]['drift']
         if not token_clean:
-            raise ValueError(f"Token {token} not found in TOKEN_MAPPING for DRIFT")
+            raise ValueError(f"Token {token} not found in tokens.TOKEN_MAPPING for DRIFT")
         s3_path = f"s3://iamjakkie-public/drift/{token_clean}/trades/*.parquet"
     else:
         raise ValueError("src must be one of SOLANA, ETHEREUM, BASE, DYDX, HYPERLIQUID, DRIFT")
@@ -52,17 +52,20 @@ def roof_price(df: pl.DataFrame, src: str, token: str) -> pl.DataFrame:
     """
     Cap the price of the token to its max price
     """
-    if token not in TOKEN_MAPPING:
-        raise ValueError(f"Token {token} not found in TOKEN_MAPPING")
+    if token not in tokens.TOKEN_MAPPING:
+        raise ValueError(f"Token {token} not found in tokens.TOKEN_MAPPING")
     
-    max_price = TOKEN_MAPPING[token]['max_price']
+    print(tokens.TOKEN_MAPPING[token])
+    max_price = tokens.TOKEN_MAPPING[token]['max_price']
     if max_price is None:
-        raise ValueError(f"Max price for token {token} not found in TOKEN_MAPPING")
+        raise ValueError(f"Max price for token {token} not found in tokens.TOKEN_MAPPING")
 
     if src == "SOLANA":
-        return df.with_columns(
-            pl.when(pl.col("USD_PRICE") > max_price)
-            .then(max_price)
-            .otherwise(pl.col("USD_PRICE"))
-            .alias("USD_PRICE")
-        )
+        return (df
+                .filter(pl.col("USD_PRICE").is_finite())
+                .with_columns(
+                    pl.when(pl.col("USD_PRICE") > max_price)
+                    .then(max_price)
+                    .otherwise(pl.col("USD_PRICE"))
+                    .alias("USD_PRICE")
+                ))
