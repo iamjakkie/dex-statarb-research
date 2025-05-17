c5c07851079362b4e5711e6b1f4d407824dbfa11
jakkie <jakubkielbasiewicz@gmail.com>
2025-05-17 21:49:39 +0200

    strategy invalid

--- src/strategy.py
+++ src/strategy.py
@@ -11,10 +11,9 @@ class Trade:
     timestamp: str
     type: TradeType
     side: str
-    entry_a_price: float
-    entry_b_price: float
-    a_qty: float
-    b_qty: float
+    entry_price: float
+    exit_price: float
+    qty: float
     capital: float
 
 # zscore-based strategy
@@ -23,15 +22,16 @@ def backtest_pairarb(
     col_a: str,
     col_b: str,
     z_col: str,
-    entry_z: float,
-    exit_z: float,
+    entry_threshold: float,
+    exit_threshold: float,
     initial_capital: float = 1000.0
 ) -> pd.DataFrame:
     
     capital       = initial_capital
-    position      = False
+    position      = 0
     entry_buy_px  = entry_sell_px = 0.0
     qty_buy       = qty_sell       = 0.0
+    holdings_a  = holdings_b     = 0.0
     trades        = []
 
     for _, row in df.iterrows():
@@ -40,22 +40,77 @@ def backtest_pairarb(
         px_a = row[col_a]
         px_b = row[col_b]
 
-        if not position:
+        if position == 0:
             # if entry signal - enter position
-            if z < entry_z:
-                # buy A, sell B
-                qty_buy = capital / px_a
-                qty_sell = qty_buy * px_a / px_b
-                entry_buy_px = px_a
-                entry_sell_px = px_b
-                position = True
-                trades.append((t, "BUY", qty_buy, entry_buy_px))
-            elif z > -entry_z:
-                # buy B, sell A
-                qty_sell = capital / px_b
-                qty_buy = qty_sell * px_b / px_a
-                entry_sell_px = px_b
-                entry_buy_px = px_a
-                position = True
-                trades.append((t, "SELL", qty_sell, entry_sell_px))
-            
+            if z > entry_threshold:
+                # only buy A asset
+                entry_price = px_a
+                qty = capital / entry_price
+                entry_buy_px = entry_price
+                capital -= qty * entry_price
+                position = 1
+                trade = Trade(
+                    timestamp=t,
+                    type=TradeType.ENTRY,
+                    side="BUY A",
+                    entry_price=entry_price,
+                    exit_price=0.0,
+                    qty=qty,
+                    capital=capital
+                )
+                trades.append(trade)
+
+            elif z < -entry_threshold:
+                # buy B
+                entry_price = px_b
+                qty = capital / entry_price
+                entry_buy_px = entry_price
+                capital -= qty * entry_price
+                position = -1
+                trade = Trade(
+                    timestamp=t,
+                    type=TradeType.ENTRY,
+                    side="BUY B",
+                    entry_price=entry_price,
+                    exit_price=0.0,
+                    qty=qty,
+                    capital=capital
+                )
+                trades.append(trade)
+
+        elif position == 1:
+            if z <= -exit_threshold:
+                # Exit A position
+                exit_price = px_b
+                capital += qty * exit_price
+                position = 0
+                trade = Trade(
+                    timestamp=t,
+                    type=TradeType.EXIT,
+                    side="SELL A",
+                    entry_price=entry_buy_px,
+                    exit_price=exit_price,
+                    qty=qty,
+                    capital=capital
+                )
+                trades.append(trade)
+
+        elif position == -1:
+            if z >= exit_threshold:
+                # Exit B position
+                exit_price = px_a
+                capital += qty * exit_price
+                position = 0
+                trade = Trade(
+                    timestamp=t,
+                    type=TradeType.EXIT,
+                    side="SELL B",
+                    entry_price=entry_buy_px,
+                    exit_price=exit_price,
+                    qty=qty,
+                    capital=capital
+                )
+                trades.append(trade)
+
+    trades_df = pd.DataFrame([trade.__dict__ for trade in trades])    
+    return trades_df
