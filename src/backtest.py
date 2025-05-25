26901ee50d075e015b86c80e940fa46b3376ca78
jakkie <jakubkielbasiewicz@gmail.com>
2025-05-25 23:06:22 +0200

    backtest init

--- src/backtest.py
+++ src/backtest.py
@@ -1,5 +1,51 @@
 # run backtest that outputs trades_df
 # size capital
+import features
+import metrics
+import report
+
+import pandas as pd
+
+def run_backtest(
+    strategy: callable,
+    merged_df: pd.DataFrame,
+    initial_capital: float = 1000.0
+):
+    cols = [col for col in merged_df.columns if col in ["DEX", "DYDX", "HYPERLIQUID"]]
+
+    combinations = []
+    if "DEX" in cols:
+        for perp in cols[1:]:
+            combinations.append((cols[0], perp))
+    else:
+        for perp in cols:
+            for perp2 in cols[1:]:
+                if perp != perp2:
+                    combinations.append((perp, perp2))
+    
+    for combination in combinations:
+        print(f"Running backtest for {combination[0]} vs {combination[1]}")
+
+        features_df = features.compute_pairarb_features(
+            merged_df,
+            col_a=combination[0],
+            col_b=combination[1],
+            funding_col=f"{combination[1]}_funding" if f"{combination[1]}_funding" in merged_df.columns else None
+        )
+        strategy_df = strategy(
+            features_df,
+            combination[0],
+            combination[1],
+        )
+
+        metrics_out = metrics.compute_backtest_metrics(
+            strategy_df
+        )
+
+        report.generate_backtest_report_html(
+            strategy_df,
+            metrics_out
+        )
 
 def benchmark(trades: list):
     pnl_df = pd.DataFrame([t for t in trades if t["type"] == "exit"])
