e16b23a0e6e4fa90459acde9b38b87baabb43b25
jakkie <jakubkielbasiewicz@gmail.com>
2025-06-02 11:43:49 +0200

    removed funding if not inc

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -95,10 +95,16 @@ def merge_vwaps(
     merged = None
     for name, lf in vwap_map.items():
         if name != "DEX":
-            this = (lf
-                .rename({lf.columns[1]: name, lf.columns[2]: f"{name}_funding"})
-                .select(["bucket", name, f"{name}_funding"])
-            )
+            if "funding" in lf.columns:
+                this = (lf
+                    .rename({lf.columns[1]: name, lf.columns[2]: f"{name}_funding"})
+                    .select(["bucket", name, f"{name}_funding"])
+                )
+            else:
+                this = (lf
+                    .rename({lf.columns[1]: name})
+                    .select(["bucket", name])
+                )
         else:
             this = (lf
                 .rename({lf.columns[1]: name})
